d0ab3636ae6103a4c3b18db9455bca5707692625
John Lewis <johnbchron@outlook.com>
2023-06-30 11:08:25 -0500

    make  fields public

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -2,13 +2,13 @@ pub mod siso;
 
 #[derive(Clone)]
 pub struct ChatModelParams {
-  model:             String,
-  temperature:       f32,
-  top_p:             f32,
-  stop:              Vec<String>,
-  max_tokens:        u64,
-  frequency_penalty: f32,
-  presence_penalty:  f32,
+  pub model:             String,
+  pub temperature:       f32,
+  pub top_p:             f32,
+  pub stop:              Vec<String>,
+  pub max_tokens:        u64,
+  pub frequency_penalty: f32,
+  pub presence_penalty:  f32,
 }
 
 impl Default for ChatModelParams {
